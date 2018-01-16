185cc1a2d7ceffa178a94a3a3fc4679e11391cb3
Alexandre Kaze <zekaze@users.noreply.github.com>
2018-01-16 23:08:16 +0100

    Slider et diverses corrections

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700" rel="stylesheet">
     <!-- Bootstrap -->
     <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!--slick-->
+    <!--OwlCarousel-->
     <link href="plugins/OwlCarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <!--Animate Css-->
+    <link href="plugins/animatecss/animate.css" rel="stylesheet">
     <!--FontAwesome-->
     <script src="https://use.fontawesome.com/548d529be3.js"></script>
     <link href="plugins/OwlCarousel/assets/owl.theme.default.min.css" rel="stylesheet">
@@ -19,17 +21,25 @@
 <header>
     <div class="container">
         <h1 id="enseigne"><small>MINISTÈRE DU BUDGET ET DES COMPTES PUBLICS</small>
-            DIRECTION GÉNÉRALE DU BUDGET ET DES FINANCES PUBLIQUES
+            CABINET DU MINISTRE D'ÉTAT
             <img height="74" class="hidden-xs" src="img/Sceau-du-Gabon_1.png" alt="sceau du gabon">
         </h1>
     </div>
 </header>
 <div class="container section">
-    <div id="main-carousel" class="owl-carousel owl-theme carousel" style="background-image: url('img/dgbfip.jpg'); background-size: cover; background-position: center center">
-        <div class="slide">Lorem ipsum dolor sit amet, consectetur adipisicing elit. </div>
-        <div class="slide">Amet consectetur cum debitis dignissimos </div>
-        <div class="slide">Dolore est hic mollitia numquam omnis possimus praesentium </div>
-        <div class="slide">Quae quis ratione rem sed sint suscipit vel voluptate!</div>
+    <div id="main-carousel" class="owl-carousel owl-theme carousel" style="background-image: url('img/office-phone.jpeg'); background-size: cover; background-position: center center">
+        <div class="slide">
+            <span class="headline" data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</span>
+        </div>
+        <div class="slide">
+            <span class="headline" data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">Amet consectetur cum debitis dignissimos</span>
+        </div>
+        <div class="slide">
+            <span class="headline" data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">Dolore est hic mollitia numquam omnis possimus praesentium</span>
+        </div>
+        <div class="slide">
+            <span class="headline" data-animation-in="fadeInUp" data-animation-out="animate-out fadeOut">Quae quis ratione rem sed sint suscipit vel voluptate!</span>
+        </div>
     </div>
 
     <h2 class="h2">Titre de section</h2>
@@ -79,25 +89,32 @@
             </div>
         </div>
         <div class="col-sm-6">
-            <div class="h2">Vous pouvez aussi</div>
-            <div class="row text-center">
-                <div class="col-md-6">
+            <div class="h3">Vous pouvez aussi</div>
+            <div class="flex-row">
+                <div class="flex-col small-col">
                     <i class="circle-icon fa fa-phone fa-3x"></i>
-                    <div>Nous contacter au <br>01 02 03 04</div>
                 </div>
-                <div class="col-md-6">
+                <div class="flex-col large-col">
+                    Nous contacter au 01 02 03 04
+                </div>
+            </div>
+            <div class="flex-row">
+                <div class="flex-col small-col">
                     <i class="circle-icon fa fa-building fa-2x"></i>
-                    <div>Vous présenter au Ministère du Budget et des Finances Publiques, bureau XXX</div></div>
+                </div>
+                <div class="flex-col large-col">
+                    Vous présenter au Ministère du Budget et des Finances Publiques, bureau XXX
+                </div>
             </div>
         </div>
     </div>
 
-</div>t
+</div>
 
 <footer>
     <div class="container">
         <ul class="footer_menu">
-            <li>&copy; <?php echo date("Y");?> DGBFiP</li>
+            <li>&copy; <?php echo date("Y");?> MINISTERE DU BUDGET ET DES FINANCES PUBLIQUES</li>
         </ul>
     </div>
 </footer>
--- js/script.js
+++ js/script.js
@@ -1,7 +1,41 @@
 $(document).ready(function(){
-    $('.carousel').owlCarousel({
+    var owl = $('.carousel');
+
+    owl.owlCarousel({
         items: 1,
         loop: true,
-        autoplay: true
+        autoplay: true,
+        dots: false
+    });
+
+    // add animate.css class(es) to the elements to be animated
+    function setAnimation ( _elem, _InOut ) {
+        // Store all animationend event name in a string.
+        // cf animate.css documentation
+        var animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+        _elem.each ( function () {
+            var $elem = $(this);
+            var $animationType = 'animated ' + $elem.data( 'animation-' + _InOut );
+
+            $elem.addClass($animationType).one(animationEndEvent, function () {
+                $elem.removeClass($animationType); // remove animate.css Class at the end of the animations
+            });
+        });
+    }
+
+// Fired before current slide change
+    owl.on('change.owl.carousel', function(event) {
+        var $currentItem = $('.owl-item', owl).eq(event.item.index);
+        var $elemsToanim = $currentItem.find("[data-animation-out]");
+        setAnimation ($elemsToanim, 'out');
+    });
+
+// Fired after current slide has been changed
+    owl.on('changed.owl.carousel', function(event) {
+
+        var $currentItem = $('.owl-item', owl).eq(event.item.index);
+        var $elemsToanim = $currentItem.find("[data-animation-in]");
+        setAnimation ($elemsToanim, 'in');
     })
 });
